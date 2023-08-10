eb118ec293021a2dd9840fd61863480055e57c29
Marina Druzhinina <mod.1@bk.ru>
2023-08-10 08:15:31 +0400

    seminar3

---
+++ CarApp/src/Classes/Pickup.java
@@ -0,0 +1,38 @@
+package Classes;
+
+import java.awt.Color;
+
+public class Pickup extends Car implements iRefueling {
+
+    private int loadCapacity;
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    public Pickup(String make, 
+                String model, 
+                Color color, 
+                TypeCar bodyType, 
+                int numberWheels, 
+                TypeFuel fuel,
+                TypeGearbox gearbox, 
+                float engineCap) {
+        super(make, model, color, bodyType, numberWheels, fuel, gearbox, engineCap);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public int gearShift(int gear) {
+         return 0;
+    }
+
+    @Override
+    public void fuel() {
+     
+    }
+    
+}
